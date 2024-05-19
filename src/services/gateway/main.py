7a0fb4975cb99ccb9b2c0287eb60bfef737f230b
Francisco Pereira <franciscopere987@gmail.com>
2024-05-19 20:20:15 -0300

    ADD: /users endpoint integration to services/gateway

--- src/services/gateway/main.py
+++ src/services/gateway/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Body, FastAPI, Response, Request
 from src.model.gateway import HelloResponse
-from typing import Dict
+from fastapi.responses import JSONResponse
+import requests as r
+
+from src.model.users.auth_request import AuthRequest
+from src.model.users.user_data import UserData
+
 
 app = FastAPI()
 
@@ -9,4 +15,14 @@ app = FastAPI()
 async def hello(name: str) -> HelloResponse:
    return HelloResponse(name=f"Hello, {name}") 
 
+@app.get("/users/health")
+async def users_health(response: Response):
+    users_response = r.get("http://users/health")
+    response.status_code = users_response.status_code
 
+@app.post("/users")
+async def get_data(request: Annotated[AuthRequest, Body()]) -> UserData:
+    users_response = r.post("http://users/users", data=request.model_dump_json())
+    
+    return users_response.json()
+    
