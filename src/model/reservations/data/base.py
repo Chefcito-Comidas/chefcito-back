0cedec0e464c14d0ff78e38f8ebb1dd9cf66ba7e
Francisco Pereira <franciscopere987@gmail.com>
2024-06-17 21:55:48 -0300

    ADD: database implementation for recovering reservations by id, updating reservations and storing new ones; ADD: Integration testing for the mentioned funcionality

--- src/model/reservations/data/base.py
+++ src/model/reservations/data/base.py
@@ -1,6 +1,10 @@
 from typing import List
+from sqlalchemy.orm import Session
 from src.model.reservations.data.schema import ReservationSchema
+from sqlalchemy import create_engine, select, update
 
+# TODO: try to add this to configuration options
+DEFAULT_POOL_SIZE = 10
 
 class ReservationsBase:
     
@@ -8,17 +12,53 @@ class ReservationsBase:
         """
             Stores a reservation on the base, 
             if the reservation is already stored then 
-            updates the reservation value
+            fails
         """
         raise Exception("Interface method should not be called")
-
+    
+    def update_reservation(self, reservation: ReservationSchema) -> None:
+        """
+            Updates information about a reservation
+        """
+        raise Exception("Interface method should not be called")
+        
     def get_reservation_by_id(self, id: str) -> ReservationSchema | None:
+        """
+            Searches for a reservation based on the id 
+            given
+        """
         raise Exception("Interface method should not be called")
 
 
 class RelBase(ReservationsBase):
-    pass
 
+    def __init__(self, conn_string: str, **kwargs):
+        kwargs["pool_size"] = kwargs.get("pool_size", DEFAULT_POOL_SIZE)
+        self.__engine = create_engine(conn_string, **kwargs)
+    
+    def store_reservation(self, reservation: ReservationSchema) -> None:
+       session = Session(self.__engine)
+       session.add(reservation)
+       session.commit()
+       session.close()
+
+    def update_reservation(self, reservation: ReservationSchema) -> None:
+        session = Session(self.__engine)
+        value = session.get(ReservationSchema, reservation.id)
+        if not value:
+            return
+        value.status = reservation.status
+        value.time = reservation.time
+        value.people = reservation.people
+        session.commit()
+        session.close()
+
+    def get_reservation_by_id(self, id: str) -> ReservationSchema | None:
+       session = Session(self.__engine)
+       query = select(ReservationSchema).where(ReservationSchema.id.__eq__(id))
+       result = session.scalar(query)
+       session.close()
+       return result
 
 class MockBase(ReservationsBase):
 
@@ -26,11 +66,17 @@ class MockBase(ReservationsBase):
         self.base: List[ReservationSchema] = []
 
     def store_reservation(self, reservation: ReservationSchema) -> None:
+        for stored in self.base:
+            if stored.id == reservation.id:
+                raise Exception("Reservation already exists")
+        self.base.append(reservation)
+    
+    
+    def update_reservation(self, reservation: ReservationSchema) -> None:
         for index, stored in enumerate(self.base):
             if stored.id == reservation.id:
                 self.base[index] = reservation
                 return
-        self.base.append(reservation)
 
     def get_reservation_by_id(self, id: str) -> ReservationSchema | None:
         for stored in self.base:
---
+++ test/services/test_reservations.py
@@ -0,0 +1,32 @@
+import pytest
+from testcontainers.postgres import PostgresContainer
+from src.model.reservations.data.base import RelBase
+from src.model.reservations.reservation import Accepted, create_reservation
+from src.model.reservations.update import Update
+from test.services.db_load import run
+
+
+@pytest.mark.asyncio
+async def test_reservation_persistance():
+    with PostgresContainer('postgres:16') as postgres:
+        run('db_config.yaml', connection=postgres.get_connection_url()) 
+        reservation = create_reservation(user="user",venue="venus",time="at",people=3)
+        database = RelBase(conn_string=postgres.get_connection_url())
+        database.store_reservation(reservation.persistance())
+        result = database.get_reservation_by_id(reservation.id)
+        assert result != None
+        assert result.id == reservation.id
+
+@pytest.mark.asyncio
+async def test_reservation_update():
+    with PostgresContainer('postgres:16') as postgres:
+        run('db_config.yaml', connection=postgres.get_connection_url()) 
+        reservation = create_reservation(user="user", venue="venue",time="at",people=4)
+        database = RelBase(conn_string=postgres.get_connection_url())
+        database.store_reservation(reservation.persistance())
+        update = Update(user="venue", accept=True)
+        reservation = update.modify(reservation)
+        database.update_reservation(reservation.persistance())
+        result = database.get_reservation_by_id(reservation.id)
+        assert result != None
+        assert result.status == Accepted().get_status()
