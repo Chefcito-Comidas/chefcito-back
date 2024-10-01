af8b69c15228396cb185b65013a18d7740f16547
Francisco Pereira <franciscopere987@gmail.com>
2024-10-01 20:07:10 -0300

    LOGFIX: query reservations

--- src/model/reservations/reservationQuery.py
+++ src/model/reservations/reservationQuery.py
@@ -2,6 +2,7 @@ import asyncio
 from datetime import datetime
 from typing import Dict, List, Optional, Tuple
 from pydantic import BaseModel
+from src.model.commons.logger import Logger
 from src.model.opinions.opinion import Opinion
 from src.model.opinions.opinion_query import OpinionQuery
 from src.model.opinions.provider import OpinionsProvider
@@ -40,7 +41,6 @@ class ReservationQuery(BaseModel):
         )
         for result in promises:
             value = await result
-            print(value)
             if isinstance(value, dict) and value['total'] > 0:
                 op = value['result'].pop()
                 final[op.reservation] = op
@@ -56,6 +56,7 @@ class ReservationQuery(BaseModel):
         result = await builder.get(self.id, self.user, self.status, self.venue, time, self.people, self.limit, self.start)
         reservations = [Reservation.from_schema(value) for value in result.result]
         opinions_result = await self.__search_opinions(reservations, opinions)
+        Logger.info(f"Queried reservations and obtained: {reservations}, {opinions_result}")
         return ReservationQueryResponse(result=reservations, 
                                         opinions=opinions_result,
                                         total=result.total)
