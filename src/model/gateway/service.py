9ea6395daab42949211409031b494bd7d6f4e088
Francisco Pereira <franciscopere987@gmail.com>
2024-12-07 15:58:06 -0300

    HOTFIX: New restaurants IDs

--- src/model/gateway/service.py
+++ src/model/gateway/service.py
@@ -30,7 +30,7 @@ from src.model.venues.update import Update
 import src.model.gateway.venues_stubs as v_stubs
 import src.model.gateway.users_stubs as u_stubs
 
-PROMOTED_IDS = ["4OKypLvTt5Ndq8naZQEuQzuB9yy1", "dacHLXBxtxfAmu10xVOoHEU4T6m2", "635fLL6HWiO6OR4t5rUdSPz7ixH2"]
+PROMOTED_IDS = ["LWir3AiNH8bhv3CJuhob5uet9el1", "LWir3AiNH8bhv3CJuhob5uet9el1", "LWir3AiNH8bhv3CJuhob5uet9el1"]
 
 class GatewayService:
 
